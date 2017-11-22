936f2d426f1ee30a94447bba672beefab1a62e5e
Amaury POQUE <apoque@e2r10p20.42.fr>
2017-11-22 13:52:33 +0100

    premier commit

---
+++ get_next_line.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apoque <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/22 12:28:49 by apoque            #+#    #+#             */
+/*   Updated: 2017/11/22 13:46:34 by apoque           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+static int	ft_get_len(const int fd, char **line)
+{
+	int		i;
+	char	*buf2;
+
+	i = 0;
+	buf2 = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
+	read(fd, buf2, BUFF_SIZE);
+	buf2[BUFF_SIZE] = '\0';
+	*line = ft_strjoin(*line, buf2);
+	while (*line[i] != '\n' && *line[i] != '\0')
+		i++;
+	if (*line[i] == '\0')
+	{
+		ft_get_len(fd, line);
+	}
+	while (*line[i] != '\0')
+	{
+		*line[i] = '\0';
+		i++;
+	}
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	*line = "";
+	ft_get_len(fd, **line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apoque <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/22 12:24:41 by apoque            #+#    #+#             */
+/*   Updated: 2017/11/22 13:19:05 by apoque           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 1
+# include "libft/libft.a"
+
+int	get_next_line(const int fd, char **line);
+
+#endif
